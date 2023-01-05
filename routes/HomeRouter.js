cf0751f820b332161d421d532ea5e29aaa7341f1
hckim96 <hyeoncheolkim96@gmail.com>
2023-01-05 12:56:23 +0900

    날짜

--- routes/HomeRouter.js
+++ routes/HomeRouter.js
@@ -11,7 +11,15 @@ router.get('/', (req, res) => {
     return res.redirect('/?page=1');
   }
   const ip = req.headers['x-forwarded-for'] ||  req.connection.remoteAddress;
-  console.log(`ip: ${ip}`)
+  
+  let day = ['일', '월', '화', '수', '목', '금', '토'];
+  let today = new Date();
+  //1번 포맷
+  let dateFormat1 = today.getFullYear() + '년 ' + (today.getMonth()+1) + '월 '
+    + today.getDate() + '일 ' + day[today.getDay()] + '요일 '
+    + today.getHours() + '시 ' + today.getMinutes() + '분 '
+    + today.getSeconds() + '초'
+  console.log(`ip: ${ip}, ${dateFormat1}`)
   
   var searchFilter = {};
   if (q && q.length > 0) {
